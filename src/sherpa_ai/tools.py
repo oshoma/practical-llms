b1fcdca2e7df44f64601d27c6b11dd7e6fdb598b
Oshoma Momoh <oshoma@gmail.com>
2023-11-14 15:53:20 -0500

    Tweak formatting and logging in Hugchat tool

--- src/sherpa_ai/tools.py
+++ src/sherpa_ai/tools.py
@@ -38,7 +38,7 @@ def get_tools(memory):
     if cfg.HUGCHAT_EMAIL is not None and cfg.HUGCHAT_PASS is not None:
         tools.append(HugChatTool())
     else:
-        logger.warning(
+        logger.info(
             "No Hugchat email and pass in environment variables, skipping Hugchat tool"
         )
     return tools
